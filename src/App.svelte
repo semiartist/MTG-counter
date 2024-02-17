f3dfd3d47d248b4c01b327ae1733ef3bf543b1d6
semiartist <semiartist@ymail.com>
2024-02-17 00:31:39 -0600

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,45 @@
+<script>
+  import ScoreBoard from "./components/ScoreBoard.svelte";
+
+  let teams = [{name: "Red", score: 20, color:"red"}, {name: "Blue", score: 20, color:"blue"}]
+  
+  const resetGame = () =>{
+    teams = [{name: "Red", score: 20, color:"red"}, {name: "Blue", score: 20, color:"blue"}]
+  }
+
+
+
+</script>
+<div class="container">
+
+  <h1>MTG Counter App</h1>
+  <div class="score-board">
+    <ScoreBoard team={teams[0]} />
+    <ScoreBoard team={teams[1]} />
+
+  </div>
+  
+  <button on:click={resetGame}>Reset Game</button>
+  
+</div>
+
+
+<style>
+  .container {
+    width: 1000px
+  }
+  button{
+    width: 100%
+  }
+
+  .score-board{
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items:stretch ;
+    padding-bottom: 20px;
+  }
+  button {
+    background-color: yellow;
+  }
+</style>
---
+++ src/components/ScoreBoard.svelte
@@ -0,0 +1,44 @@
+<script>
+    export let team ;
+
+    const addPoint = () =>{
+        team.score +=1;
+    }
+
+    const minusPoint = () =>{
+        team.score -= 1;
+        checkScore()
+    }
+
+    const checkScore = () =>{
+
+      if (team.score === 0) {
+        alert(team.name+" team you lose!")
+      
+    }
+  }
+</script>
+
+
+<div class="board" style="border-color:{team.color}">
+    <h3>Team {team.name}: {team.score}</h3>
+    <div class="button">
+        <button class="plus" on:click={addPoint}>+</button>
+        <button class="minus" on:click={minusPoint}>-</button>
+    </div>
+</div>
+
+
+<style>
+    .board {
+        flex: 1;
+        border: dashed;
+        padding-bottom: 20px;
+    }
+    .plus {
+        background-color: green;
+    }
+    .minus {
+        background-color: red;
+    }
+</style>
